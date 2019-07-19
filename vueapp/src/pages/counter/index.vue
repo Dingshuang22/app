a4eb947d847d64f3c72c80f236f645c843117dcc
dingshuang <Dingshuang>
2019-07-19 09:38:29 +0800

    ds1

---
+++ vueapp/src/pages/counter/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>第二页面</h2>
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store'
+
+export default {
+  computed: {
+   
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vueapp/src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>首页</h2>
+  </div>
+</template>
+
+<script>
+import { formatTime } from '@/utils/index'
+import card from '@/components/card'
+
+export default {
+  components: {
+    card
+  }
+
+  
+}
+</script>
+
+<style>
+
+</style>
